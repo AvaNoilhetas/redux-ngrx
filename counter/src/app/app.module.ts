826edaa2203d9acb4b9474ee8983f037c5ff5b36
AvaNoilhetas <a.noilhetas@hotmail.fr>
2022-12-07 22:04:03 +0100

    Add NgRx devtool

--- counter/src/app/app.module.ts
+++ counter/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { counterReducer } from './counter.reducer';
 import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ counter: counterReducer })],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
